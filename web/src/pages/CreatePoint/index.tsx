a9becb84cb34731bfa121c785cfd193464cd9978
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-03 15:02:06 -0300

    States and Items API calls

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
+import api from '../../services/api';
+import axios from 'axios';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string
+}
+
+interface IBGEUFResponse {
+    id: number;
+    nome: string;
+}
+
+interface State {
+    id: number;
+    name: string,
+}
 
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+    const [states, setStates] = useState<State[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const statesNames = response.data.map(state => {
+                return {
+                    id: state.id,
+                    name: state.nome
+                }
+            });
+            setStates(statesNames);
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -63,6 +102,9 @@ const CreatePoint = () => {
                             <label htmlFor="state">State</label>
                             <select name="state" id="state">
                                 <option value="0">Select state</option>
+                                {states.map(state => (
+                                    <option key={state.id} value={state.name}>{state.name}</option>
+                                ))}
                             </select>
                         </div>
 
@@ -82,30 +124,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
-                            <span>Cooking Oil</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
 
