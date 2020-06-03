91b54efeab541d5abbf280dc4ee30ac763ad077c
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-03 11:37:14 -0300

    Rename Pages Folder

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//import './styles.css';
+//import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+    return <h1>create point</h1>
+}
+
+export default CreatePoint;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
-//import Home from './pages/Home';
 
 import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Route component={Home} path="/" />
+            <Route component={Home} path="/" exact />
+            <Route component={CreatePoint} path="/create-point" />
         </BrowserRouter>
     );
 }
