7676f0b41522b6f60ee8edc69d36f53a919a1811
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-03 11:29:44 -0300

    Router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './Pages/Home';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <Home/>
-  
+    <Routes />
+
 
   );
 }
