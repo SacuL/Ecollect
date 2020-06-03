fa3c309e59a8ba057c1d6ab37921bd91484d4d6e
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-03 16:25:31 -0300

    Save point to backend

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import api from '../../services/api';
@@ -52,6 +52,8 @@ const CreatePoint = () => {
         email: '',
         whatsapp: ''
     })
+
+    const history = useHistory();
 
 
     useEffect(() => {
@@ -136,6 +138,30 @@ const CreatePoint = () => {
         }
     }
 
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const state = selectedState.name;
+        const city = selectedCity.name;
+        const [latitude, longitude] = selectedMapPosition;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            state,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points', data);
+
+        history.push('/');
+    }
 
 
     return (
@@ -148,7 +174,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form >
+            <form onSubmit={handleSubmit}>
                 <h1> Create collection point </h1>
 
                 <fieldset>
