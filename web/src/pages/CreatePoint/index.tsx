026c0e708eaffceea90e4b84af98df30e890e601
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-03 12:04:19 -0300

    Create colletion point page (HTML)

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,109 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
-//import './styles.css';
-//import logo from '../../assets/logo.svg';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
 
 const CreatePoint = () => {
-    return <h1>create point</h1>
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecollect" />
+                <Link to="/">
+                    <FiArrowLeft />
+                    Back to home
+                </Link>
+            </header>
+
+            <form>
+                <h1> Create collection point </h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Data</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input type="text" name="whatsapp" id="whatsapp" />
+                        </div>
+                    </div>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Address</h2>
+                        <span>Select address on the map</span>
+                    </legend>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="state">State</label>
+                            <select name="state" id="state">
+                                <option value="0">Select state</option>
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">City</label>
+                            <select name="city" id="city">
+                                <option value="0">Select City</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Waste collection items</h2>
+                        <span>Select one or more items below</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oil.svg" alt="Test" />
+                            <span>Cooking Oil</span>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type="submit">Save collection point</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreatePoint;
