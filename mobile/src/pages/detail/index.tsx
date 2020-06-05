76c9c2d13f85450fd6f5fa2e6e1d376bd788f978
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-05 09:21:49 -0300

    Whatsapp and email deeplinking

--- mobile/src/pages/detail/index.tsx
+++ mobile/src/pages/detail/index.tsx
@@ -4,6 +4,8 @@ import { View, StyleSheet, Text, TouchableOpacity, Image, SafeAreaView } from 'r
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler'
 import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
+import { Linking } from 'expo';
 
 interface Params {
     point_id: number;
@@ -33,6 +35,17 @@ const Detail = () => {
 
     function handleNavigateBack() {
         navigation.goBack();
+    }
+
+    function handleComposeMail() {
+        MailComposer.composeAsync({
+            subject: 'Waste collection',
+            recipients: [data.point.email]
+        });
+    }
+
+    function handleWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}`);
     }
 
     useEffect(() => {
@@ -69,12 +82,12 @@ const Detail = () => {
             </View>
 
             <View style={styles.footer} >
-                <RectButton style={styles.button}>
+                <RectButton style={styles.button} onPress={handleWhatsapp}>
                     <FontAwesome name="whatsapp" size={20} color="#FFF"></FontAwesome>
                     <Text style={styles.buttonText}>Whatsapp</Text>
                 </RectButton>
 
-                <RectButton style={styles.button}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} color="#FFF"></Icon>
                     <Text style={styles.buttonText}>Email</Text>
                 </RectButton>
@@ -136,7 +149,7 @@ const styles = StyleSheet.create({
         borderColor: '#999',
         paddingVertical: 20,
         paddingHorizontal: 32,
-        paddingBottom: 0,
+
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
