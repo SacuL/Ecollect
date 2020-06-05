99472c0b5de8268ce6aae2e37bdfffd2ed355a1b
Lucas Cerqueira <lucas44r@gmail.com>
2020-06-05 09:00:03 -0300

    Home page

---
+++ mobile/src/pages/home/index.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, Image, StyleSheet, Text, ImageBackground, SafeAreaView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+const Home = () => {
+
+    const navigation = useNavigation();
+
+    function handleNavigateToPoints() {
+        navigation.navigate("Points");
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <ImageBackground
+                style={styles.container}
+                source={require('../../../assets/home-background.png')}
+                imageStyle={{ width: 274, height: 368 }} >
+
+                <View style={styles.main}>
+                    <Image source={require('../../../assets/logo.png')} />
+                    <Text style={styles.title}>Your waste collection marketplace.</Text>
+                    <Text style={styles.description}>Find collection points efficiently.</Text>
+                </View>
+
+
+                <View style={styles.footer}>
+                    <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+                        <View style={styles.buttonIcon}>
+                            <Text>
+                                <Icon name="arrow-right" color="#FFF" size={24} />
+                            </Text>
+                        </View>
+                        <Text style={styles.buttonText}>
+                            Join
+                        </Text>
+                    </RectButton>
+                </View>
+
+            </ImageBackground>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 32
+    },
+
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    title: {
+        color: '#322153',
+        fontSize: 32,
+        fontFamily: 'Ubuntu_700Bold',
+        maxWidth: 260,
+        marginTop: 64,
+    },
+
+    description: {
+        color: '#6C6C80',
+        fontSize: 16,
+        marginTop: 16,
+        fontFamily: 'Roboto_400Regular',
+        maxWidth: 260,
+        lineHeight: 24,
+    },
+
+    footer: {},
+
+    select: {},
+
+    input: {
+        height: 60,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        marginBottom: 8,
+        paddingHorizontal: 24,
+        fontSize: 16,
+    },
+
+    button: {
+        backgroundColor: '#34CB79',
+        height: 60,
+        flexDirection: 'row',
+        borderRadius: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        marginTop: 8,
+    },
+
+    buttonIcon: {
+        height: 60,
+        width: 60,
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        color: '#FFF',
+        fontFamily: 'Roboto_500Medium',
+        fontSize: 20,
+    }
+});
+
+export default Home;
