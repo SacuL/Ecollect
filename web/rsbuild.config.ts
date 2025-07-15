24136c953fe92771d4b99c86bce258e9030c5bac
Lucas Cerqueira <lucas44r@gmail.com>
2025-07-15 14:35:34 -0300

    Migrate from Create React App to rsbuild

---
+++ web/rsbuild.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: './public/index.html',
+  },
+  output: {
+    distPath: {
+      root: 'build',
+    },
+  },
+});
